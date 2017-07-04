762f9498ba89587f533c9a03070fe333dab97e23
dhruv-dj <dhruv2703@gmail.com>
2017-07-04 10:40:27 +0530

    Secure Links

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var quote;
     function getQuote(){
     $.ajax({
-       url: 'http://api.forismatic.com/api/1.0/',
+       url: 'https://api.forismatic.com/api/1.0/',
         jsonp : 'jsonp',
         dataType : 'jsonp',
         data : {
