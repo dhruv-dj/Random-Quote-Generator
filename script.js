4ecf5ecb21813d480000f50a853331cfd2f7f0ad
dhruv-dj <dhruv2703@gmail.com>
2017-07-04 10:35:31 +0530

    App Files
    HTML and JS File

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var author;
+    var quote;
+    function getQuote(){
+    $.ajax({
+       url: 'http://api.forismatic.com/api/1.0/',
+        jsonp : 'jsonp',
+        dataType : 'jsonp',
+        data : {
+            method : 'getQuote',
+            format : 'jsonp',
+            lang : 'en'            
+        },
+        success : function(parameter){
+            author = parameter.quoteAuthor;
+            quote = parameter.quoteText;
+            $('#quote').text(quote);
+            $('#author').text("-" + author);
+        }
+    });
+    }
+    getQuote();
+    $('#newQuote').on('click',function(event){
+        event.preventDefault();
+        getQuote();
+    });
+    $('#tweet').on('click',function(event){
+        event.preventDefault();
+        window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(quote + '-- ' + author));
+    });
+});
